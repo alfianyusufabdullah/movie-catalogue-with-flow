dcbd742ee18f57ab63dc914726e9c010cfdd7c67
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-02-04 20:05:01 +0700

    create movie ViewModel

---
+++ app/src/main/java/alfianyusufabdullah/moviecatalogue/screen/MovieViewModel.kt
@@ -0,0 +1,49 @@
+package alfianyusufabdullah.moviecatalogue.screen
+
+import alfianyusufabdullah.moviecatalogue.common.Mapper
+import alfianyusufabdullah.moviecatalogue.common.ScreenType
+import alfianyusufabdullah.moviecatalogue.data.repository.MovieRepositoryImpl
+import alfianyusufabdullah.moviecatalogue.entity.Movie
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+@ExperimentalCoroutinesApi
+class MovieViewModel(private val repository: MovieRepositoryImpl, private val mapper: Mapper) :
+    ViewModel() {
+
+    private val _movies = MutableLiveData<List<Movie>>()
+    private val _loading = MutableLiveData<Boolean>()
+    private val _error = MutableLiveData<String>()
+
+    val movies: LiveData<List<Movie>>
+        get() = _movies
+
+    val loading: LiveData<Boolean>
+        get() = _loading
+
+    val error: LiveData<String>
+        get() = _error
+
+    @ExperimentalCoroutinesApi
+    fun loadMovie(screenType: ScreenType) {
+
+        viewModelScope.launch {
+            repository.loadMovie(screenType)
+                .onStart { _loading.postValue(true) }
+                .map { mapper.nullOnListItemMapper(it) }
+                .map { mapper.movieUrlImageMapper(it) }
+                .map { mapper.movieNameMapper(it) }
+                .map { mapper.movieDateMapper(it) }
+                .catch { _error.postValue(it.message) }
+                .onCompletion { _loading.postValue(false) }
+                .collect { _movies.postValue(it) }
+        }
+    }
+}
